dad119797795880388497a1640f00f71749ad1f7
Alexander Kazakov <kebagami@mail.ru>
2022-07-18 22:36:51 +0300

    Task 1 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -15,7 +15,8 @@ function calcShipping(sum, min, shipping) {
 
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
-
+     let  shipping;
+     
     // Конец решения задания №2.1.
 
     return shippingSum;
